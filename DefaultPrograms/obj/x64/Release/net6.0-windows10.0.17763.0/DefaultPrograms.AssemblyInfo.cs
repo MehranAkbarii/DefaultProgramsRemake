4006f76fa96676b9b3b784c786be6c631bd4e59c
mehraanakbarii <120687618+mehraanakbarii@users.noreply.github.com>
2024-04-21 02:11:58 +0330

    -v1.1.3

--- DefaultPrograms/obj/x64/Release/net6.0-windows10.0.17763.0/DefaultPrograms.AssemblyInfo.cs
+++ DefaultPrograms/obj/x64/Release/net6.0-windows10.0.17763.0/DefaultPrograms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DefaultPrograms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a02f5258c936ac2b9412ad4d04dce909fc7a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+777797dd97c09ea54f4c15a9c4bba66f668cfd19")]
 [assembly: System.Reflection.AssemblyProductAttribute("DefaultPrograms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DefaultPrograms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
